d7f176de54e3f752dd0df9d2533722b22687a418
Tanawat Horsirimanon <galiblenight@gmail.com>
2020-04-29 02:53:03 +0800

    initialize

---
+++ test1/target/release/deps/test1-069c22c959ef5668.d
@@ -0,0 +1,5 @@
+/root/test1/target/release/deps/test1-069c22c959ef5668: src/main.rs
+
+/root/test1/target/release/deps/test1-069c22c959ef5668.d: src/main.rs
+
+src/main.rs:
---
+++ test1/target/release/test1.d
@@ -0,0 +1,1 @@
+/root/test1/target/release/test1: /root/test1/src/main.rs
